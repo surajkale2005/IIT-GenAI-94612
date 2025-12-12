8dec419ce21e0f51eefd9b4e81dcf4f739f4ea37
rajvardhan kale <rajvardhankale.r@gmail.com>
2025-12-12 10:28:28 +0530

    add requests and pandas

---
+++ ass2/Exercise2/app.py
@@ -0,0 +1,8 @@
+import requests
+import pandas
+
+print("Hello Project A")
+print("requests version: ", requests.__version__)
+
+print("Hello Project A")
+print("pandas version: ", pandas.__version__)
