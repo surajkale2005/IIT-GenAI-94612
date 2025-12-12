ebd9088a7e50e3f666a92e8852ecba55e4f0e643
rajvardhan kale <rajvardhankale.r@gmail.com>
2025-12-12 08:53:59 +0530

    add day2

---
+++ day2/main.py
@@ -0,0 +1,9 @@
+import calculator
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+print("Addition:", calculator.add(a, b))
+print("Subtraction:", calculator.subtract(a, b))
+print("Multiplication:", calculator.multiply(a, b))
+print("Division:", calculator.divide(a, b))
